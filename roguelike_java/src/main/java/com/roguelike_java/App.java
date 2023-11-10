b9350c33957d3d655802e196b985a28c86401e6e
Ertiks <ertiks42@yahoo.fr>
2023-11-10 09:59:57 +0100

    Nettoyage du projet de base
    Juste virer les controllers inutiles et les imports qu'on ne comprend pas.

--- roguelike_java/src/main/java/com/roguelike_java/App.java
+++ roguelike_java/src/main/java/com/roguelike_java/App.java
@@ -1,10 +1,9 @@
 package com.roguelike_java;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
@@ -14,21 +13,15 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private Pane root;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        this.root = new Pane();
+        this.scene = new Scene(root, 1280, 720);
+
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {

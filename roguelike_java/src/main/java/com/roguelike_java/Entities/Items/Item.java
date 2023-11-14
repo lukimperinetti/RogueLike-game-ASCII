6dcd155fae15840f60aa6313e2706e19b7373535
lukimperinetti <luk.imperinetti@epitech.eu>
2023-11-14 12:08:43 +0100

    réccupération de code

--- roguelike_java/src/main/java/com/roguelike_java/Entities/Items/Item.java
+++ roguelike_java/src/main/java/com/roguelike_java/Entities/Items/Item.java
@@ -1,10 +1,12 @@
 package com.roguelike_java.Entities.Items;
 
+import com.roguelike_java.App;
 import com.roguelike_java.Entities.Entity;
 
 public class Item extends Entity{
     
     public Item(String name, int X, int Y, String imageName, String tag){
         super(name, X, Y, "Sword.png", tag);
+        App.displaySprite(this);
     }
 }
